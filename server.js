da43ae26d352d18e6d5a518bf048d974c2241fbd
adnanalemran <adnanalemranontor@gmail.com>
2024-08-20 18:04:27 +0600

    chore: Update npm dependencies and add Express server
    - Update npm dependencies to the latest stable versions
    - Add Express server configuration in server.js file
    - Serve static files from the "public" directory

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const next = require("next");
+const path = require("path");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  server.use("/public", express.static(path.join(__dirname, "public")));
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});

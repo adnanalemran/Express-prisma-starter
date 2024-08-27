549373cb1883b39fef979ea36e1f00e1d03769a1
adnanalemran <adnanalemranontor@gmail.com>
2024-08-27 18:47:57 +0600

    Update lazy loading and responsive styles in multiple components

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,18 @@
+import SecTitle from "@/components/common/SecTitle";
 import React from "react";
 
 const page = () => {
-  return <div className="min-h-100"></div>;
+  return (
+    <div className="min-h-100 container mx-auto pt-16 ">
+      <div className="text-xl text-slate-600">Let’s work together</div>
+      <SecTitle
+        title="We bring a wealth off experience 
+from a wide range of backgrounds"
+        subtitle="Our philosophy is simple; hire great people and give them
+the resources and support to do their best work."
+      />
+    </div>
+  );
 };
 
 export default page;

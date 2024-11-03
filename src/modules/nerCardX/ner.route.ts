94a3675b88bf12eb95681b6e84087e9c68292d2e
adnanalemran <adnanalemranontor@gmail.com>
2024-11-03 17:28:07 +0600

    final 1.o

--- src/modules/nerCardX/ner.route.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { NerController } from "./ner.controller";
-const router = express.Router();
-
-router.post("/extract-data", NerController.extractData);
-
-export const NerRoute = router;
